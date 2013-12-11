c65c944d73c3268290bb08080be056cf08f15ae8
Robin Winslow <robin@robinwinslow.co.uk>
2013-12-11 23:56:51 +0000

    The polls app (so far)

---
+++ polls/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from polls.models import Poll
+from polls.models import Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question']
+
+admin.site.register(Poll, PollAdmin)
