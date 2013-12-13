fc34120cf1b2bb0524f0642185680fd7761aba02
Robin Winslow <robin@robinwinslow.co.uk>
2013-12-13 17:03:17 +0000

    Create tests and fix was_published_recently

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,38 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from polls.models import Poll
+
+class PollMethodTests(TestCase):
+    def test_was_published_recently_with_future_poll(self):
+        """
+        was_published_recently() should return False for polls whose
+        pub_date is in the future
+        """
+        future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
+        self.assertEqual(future_poll.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_poll(self):
+        """
+        was_published_recently() should return False for old polls
+        """
+        past_poll = Poll(pub_date=timezone.now() - datetime.timedelta(days=1, microseconds=1))
+        self.assertEqual(past_poll.was_published_recently(), False)
+
+    def test_was_published_recently_with_new_poll(self):
+        """
+        was_published_recently() should return True for polls whose
+        pub_date is now
+        """
+        new_poll = Poll(pub_date=timezone.now())
+        self.assertEqual(new_poll.was_published_recently(), True)
+
+    def test_was_published_recently_with_recent_poll(self):
+        """
+        was_published_recently() should return True for polls
+        published within the last day
+        """
+        recent_poll = Poll(pub_date=timezone.now() - datetime.timedelta(days=1, seconds=-1))
+        self.assertEqual(recent_poll.was_published_recently(), True)
