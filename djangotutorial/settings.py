07ae6aaf7746f6bd6bcb28630f34b492516fbb38
Robin Winslow <robin@robinwinslow.co.uk>
2013-12-27 01:46:13 +0000

    Use sqlite as a backup db config, but use database url if it exists - e.g. from heroku

--- djangotutorial/settings.py
+++ djangotutorial/settings.py
@@ -55,16 +55,15 @@ WSGI_APPLICATION = 'djangotutorial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dqccoqu2ge2ct',
-        'USER': 'ggwgkdkddexpdi',
-        'PASSWORD': 'yrAaKq_GiKt_P29yNNCdK5AGe3',
-        'HOST': 'ec2-54-221-243-6.compute-1.amazonaws.com',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+if len(dj_database_url.config().keys()) != 0:
+    DATABASES = {'default': dj_database_url.config()}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
